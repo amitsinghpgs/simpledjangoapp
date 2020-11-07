f6a0be93ea31f8c2f444e44d2c9afa86acd55239
amitsinghpgs <amitsinghpgs@gmail.com>
2020-11-07 17:57:37 +0530

    add allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ['DEBUG']==True)
 
-ALLOWED_HOSTS=['*']
+ALLOWED_HOSTS=['rocky-lake-01787.herokuapp.com']
 
 
 # Application definition
